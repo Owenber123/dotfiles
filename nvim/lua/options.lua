0cd47aa919c415a49a320afba0c66c766de7559d
Owen <beringer.owen@gmail.com>
2025-01-07 14:26:47 -0500

    Prevent no background colour notification on start

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
 -- add yours here!
-
+require("notify").setup({
+  background_colour = "#ffffffff",
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
