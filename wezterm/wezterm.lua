443423e57d17e736f23ff0b85b89ba8edd3f70dc
Owen <beringer.owen@gmail.com>
2025-06-05 16:28:42 -0400

    Wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,9 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+config.term = "xterm-256color"
 config.color_scheme = "Tokyo Night"
 config.window_close_confirmation = "NeverPrompt"
 config.enable_tab_bar = false
@@ -14,11 +11,10 @@ config.default_cursor_style = "BlinkingBar"
 config.window_decorations = "RESIZE"
 config.font = wezterm.font("JetBrains Mono")
 config.hide_tab_bar_if_only_one_tab = true
--- and finally, return the configuration to wezterm
 config.background = {
 	{
 		source = {
-			File = "/Users/owenberinger/Pictures/Background/triangles-neon-colors-8k-jx-3440x1440.jpg.png",
+		  File = "/Users/owenberinger/Pictures/Background/triangles-neon-colors-8k-jx-3440x1440.jpg.png",
 		},
 		hsb = {
 			hue = 1.0,
@@ -30,20 +26,21 @@ config.background = {
 	},
 	{
 		source = {
-			Color = "#281a37",
+      Color = "#222222",
 		},
 		width = "100%",
 		height = "100%",
 		opacity = 0.8,
+    -- opacity = 1.0,
 	},
 }
--- config.background_colour = "#281a37"
 
--- window_padding = {
---   left = 3,
---   right = 3,
---   top = 0,
---   bottom = 0,
--- }
+config.window_padding = {
+   left = 0,
+   right = 0,
+   top = 0,
+   bottom = 0,
+ }
+
 
 return config
