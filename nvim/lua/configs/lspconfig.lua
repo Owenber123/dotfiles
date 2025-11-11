297a2899ec50444f8276f4a01307ea8c070ec9ac
Owen Beringer <oberinger@lime.co>
2025-11-11 09:44:22 -0500

    require('lspconfig') depricated

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,33 +1,24 @@
--- load defaults i.e lua_lsp
+-- Load NvChad defaults (lua_ls etc.)
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
-
--- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "gopls", "zls" }
 local nvlsp = require("nvchad.configs.lspconfig")
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup({
+-- LS servers with default config
+local servers = { "html", "cssls", "ts_ls", "gopls", "zls" }
+
+for _, server in ipairs(servers) do
+    vim.lsp.config(server, {
         on_attach = nvlsp.on_attach,
         on_init = nvlsp.on_init,
         capabilities = nvlsp.capabilities,
     })
+
+    -- enable globally for this server
+    vim.lsp.enable(server)
 end
 
--- require'lspconfig'.gopls.setup {
---   on_attach = function(client, bufnr)
---     client.server_capabilities.signatureHelpProvider = false
---     on_attach(client, bufnr)
---   end,
---   capabilities = capabilities
--- }
-
-local nvim_lsp = require("lspconfig")
-
--- C++
-nvim_lsp.clangd.setup({
+-- C++: clangd
+vim.lsp.config("clangd", {
     cmd = { "clangd", "--background-index", "--compile-commands-dir=build" },
     init_options = {
         clangdFileStatus = true,
@@ -42,15 +33,17 @@ nvim_lsp.clangd.setup({
             compilationDatabasePath = "build",
             fallbackFlags = {
                 "-std=c++23",
-                "-I/opt/homebrew/include", -- macOS Homebrew installs
-                "-I/opt/homebrew/opt/boost/include", -- Boost Headers
+                "-I/opt/homebrew/include",
+                "-I/opt/homebrew/opt/boost/include",
                 "-I/opt/homebrew/opt/boost/include/boost",
             },
         },
     },
     on_attach = function(client, bufnr)
-        client.server_capabilities.signatureHelpProvider = false -- ⛔ disables signature popup
-        nvlsp.on_attach(client, bufnr) -- preserve NvChad features like formatting and keymaps
+        client.server_capabilities.signatureHelpProvider = false
+        nvlsp.on_attach(client, bufnr)
     end,
     capabilities = nvlsp.capabilities,
 })
+
+vim.lsp.enable("clangd")
